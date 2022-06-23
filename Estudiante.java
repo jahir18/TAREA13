7141a5d6b3fa327d4667c9ab839799d48b5dc350
Jahir Anthony <103447011+jahir18@users.noreply.github.com>
2022-06-23 04:25:46 -0500

    Add files via upload

---
+++ Estudiante.java
@@ -0,0 +1,32 @@
+package Herencia;
+
+public class Estudiante extends Persona {
+
+    private int codigoEstudiante;
+    private float notaFinal;
+
+
+    public Estudiante(String nombre, String apellido, int edad, int codigoEstudiante, float notaFinal){
+
+        super(nombre,apellido,edad);
+
+        this.codigoEstudiante=codigoEstudiante;
+        this.notaFinal=notaFinal;
+
+    }
+
+    public void mostrarDatos(){
+        System.out.println("Nombre: "+getNombre());
+        System.out.println("Apellido: "+getApellido());
+        System.out.println("Edad: "+getEdad());
+        System.out.println("Codigo estudiante: "+codigoEstudiante);
+        System.out.println("Nota final: "+notaFinal);
+        
+    }
+
+
+
+
+
+}
+
---
+++ Main.java
@@ -0,0 +1,13 @@
+package Herencia;
+
+public class Main {
+    public static void main(String[] args){
+
+        Estudiante estudiante= new Estudiante("JAHIR ANTHONY","TRJILLO VILCHERREZ",23,2019110764,14);
+
+        estudiante.mostrarDatos();
+    }
+
+
+    
+}
